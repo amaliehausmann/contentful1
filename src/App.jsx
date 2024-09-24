0fbd9afd822e9792b31f024cad7b11eafd27cb0a
lasse <77295879+lassejlv@users.noreply.github.com>
2024-09-24 10:29:21 +0200

    Better paths, put client into one function

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { useEffect, useState } from "react";
-import "./App.scss";
-import * as contentful from "contentful";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
+import { client } from "@/lib/contentful";
+import "./App.scss";
 
 function App() {
   const [productData, setProductData] = useState("");
-
-  const client = contentful.createClient({
-    space: import.meta.env.VITE_PUBLIC_SPACE_ID,
-    accessToken: import.meta.env.VITE_PUBLIC_ACCESS_TOKEN,
-  });
 
   useEffect(() => {
     client
@@ -27,7 +22,7 @@ function App() {
     <>
       <h2>Telefon</h2>
       {productData?.items?.map((item) => 
-        <div>
+        <div key={item.sys.id}>
           <h1>{item.fields.title}</h1>
           <img src={item.fields.image.fields.file.url} alt="" />
           {documentToReactComponents(item.fields.description)}
