1ba58455064d07ce8e47e8044fd2e7049a5370a6
amaliehausmann <amaliehausmann@gmail.com>
2024-09-24 11:05:58 +0200

    gallery view

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,31 @@
 import { useEffect, useState } from "react";
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-import { client } from "@/lib/contentful";
+import { client } from "./lib/contentful";
+import { Gallery } from "./components/Gallery/Gallery";
 import "./App.scss";
 
 function App() {
-  const [productData, setProductData] = useState("");
+  const [galleryData, setGalleryData] = useState("");
 
   useEffect(() => {
     client
-    .getEntries({
-      content_type: "product_id",
-    })
-    .then((res) => setProductData(res))
-    .catch((err) => console.error(err))
-  }, [])
+      .getEntries({
+        content_type: "gallery",
+      })
+      .then((res) => setGalleryData(res))
+      .catch((err) => console.error(err));
+  }, []);
 
-  console.log(productData)
-
+  console.log(galleryData);
 
   return (
     <>
-      <h2>Telefon</h2>
-      {productData?.items?.map((item) => 
-        <div key={item.sys.id}>
-          <h1>{item.fields.title}</h1>
-          <img src={item.fields.image.fields.file.url} alt="" />
-          {documentToReactComponents(item.fields.description)}
-        </div>
-      )}
+      {galleryData?.items?.map((item) => (
+        <Gallery
+          imageURL={item.fields.image.fields.file.url}
+          imageALT={item.fields.image.fields.title}
+          description={item.fields.description.content[0].content[0].value}
+        ></Gallery>
+      ))}
     </>
   );
 }
